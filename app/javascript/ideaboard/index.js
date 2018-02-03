c61be00c10f06e3ec0b29ad81cce647fb47a4efc
Paul Traue <ptrauejr@gmail.com>
2018-02-03 16:39:20 -0600

    Adds the basic react structure following webpacker naming conventions

---
+++ app/javascript/ideaboard/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+const ideaboard = document.querySelector('#ideaboard')
+ReactDOM.render(<App />, ideaboard)
