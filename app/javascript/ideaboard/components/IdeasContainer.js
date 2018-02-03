777d760413420735a6a81a11262a4ffccb77e909
Paul Traue <ptrauejr@gmail.com>
2018-02-03 17:31:34 -0600

    Gets the data from the api

--- app/javascript/ideaboard/components/IdeasContainer.js
+++ app/javascript/ideaboard/components/IdeasContainer.js
@@ -20,8 +20,15 @@ class IdeasContainer extends Component {
 
     render() {
         return (
-            <div className="">
-                Ideas
+            <div>
+                {this.state.ideas.map((idea) => {
+                    return (
+                        <div className="tile" key={idea.id}>
+                            <h4>{idea.title}</h4>
+                            <p>{idea.body}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
