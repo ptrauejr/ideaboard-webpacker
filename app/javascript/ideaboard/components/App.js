4fcff8280ad280d55e185b91fbc751e1778c2dc4
Paul Traue <ptrauejr@gmail.com>
2018-02-03 16:44:42 -0600

    Adds new IdeasContainer component

--- app/javascript/ideaboard/components/App.js
+++ app/javascript/ideaboard/components/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import IdeasContainer from './IdeasContainer'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <div className="App-header">
-                    <h2>Welcome to React</h2>
+                    <h1>Idea Board</h1>
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <IdeasContainer />
             </div>
         )
     }
